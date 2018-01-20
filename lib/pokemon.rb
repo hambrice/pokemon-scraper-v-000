617c43808921f4fa823f2a816528b828bc6d4f73
Brad Hambrice <hambrice@uab.edu>
2018-01-20 06:34:56 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -17,8 +17,8 @@ class Pokemon
     array = db.execute("SELECT * FROM pokemon WHERE pokemon.id = ?", id)[0]
     #binding.pry
     if array[3].class == NilClass 
-    hash[id] = id
-    hash[name] = db.execute("SELECT name FROM pokemon WHERE pokemon.id = ?", id)[0][0]
+    hash = { id: id
+    hash[name:] = db.execute("SELECT name FROM pokemon WHERE pokemon.id = ?", id)[0][0]
     hash[type] = db.execute("SELECT type FROM pokemon WHERE pokemon.id = ?", id)[0][0]
     #binding.pry
     hash[db] = db
