2a801fd7cd87a9ee3ebf2b0687cfa88fc35d3d2a
Brad Hambrice <hambrice@uab.edu>
2018-01-20 05:13:43 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -19,7 +19,6 @@ class Pokemon
     hash[:id] = id
     hash[:name] = db.execute("SELECT name FROM pokemon WHERE pokemon.id = ?", id)[0][0]
     hash[:type] = db.execute("SELECT type FROM pokemon WHERE pokemon.id = ?", id)[0][0]
-    hash[:hp] = db.execute("SELECT hp FROM pokemon WHERE pokemon.id = ?", id)[0][0]
     hash[:db] = db
     #binding.pry
     Pokemon.new(hash)
