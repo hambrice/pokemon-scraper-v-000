e81bd10f0014b6aa073648fa68dd320b476b2ae3
Brad Hambrice <hambrice@uab.edu>
2018-01-20 06:23:45 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -22,7 +22,7 @@ class Pokemon
     hash[:hp] = db.execute("SELECT hp FROM pokemon WHERE pokemon.id = ?", id)[0][0] 
     hash[:db] = db
     #binding.pry
-    Pokemon.new(name: array[0], type: array[1], hp:array[2], db)
+    Pokemon.new(hash)
   end
   
   def alter_hp(hp, db)
